21e0f7f6f0205e89bd68190efe1d342db24aad26
tobiaszimmermann <cph-tz11@cphbusiness.dk>
2021-02-04 22:34:44 +0100

    tests done

---
+++ src/main/java/facades/Populator.java
@@ -0,0 +1,22 @@
+package facades;
+
+import dtos.EmployeeDTO;
+import entities.Employee;
+import javax.persistence.EntityManagerFactory;
+import utils.EMF_Creator;
+
+public class Populator {
+
+	public static void populate() {
+		EntityManagerFactory emf = EMF_Creator.createEntityManagerFactory();
+		EmployeeFacade ef = EmployeeFacade.getEmployeeFacade(emf);
+		ef.createEmployee(new EmployeeDTO("Bob", "bobvej"), 100);
+		ef.createEmployee(new EmployeeDTO("Bob", "bobvej"), 100);
+		ef.createEmployee(new EmployeeDTO("Bob", "bobvej"), 100);
+	}
+
+	public static void main(String[] args) {
+		populate();
+	}
+
+}
--- src/test/java/facades/EmployeeFacadeIT.java
+++ src/test/java/facades/EmployeeFacadeIT.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package facades;
 
 import dtos.EmployeeDTO;
+import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -33,8 +30,13 @@ public class EmployeeFacadeIT {
 	public static void tearDownClass() {
 	}
 
+	EmployeeFacade instance;
+	EntityManagerFactory emf;
+
 	@BeforeEach
 	public void setUp() {
+		emf = Persistence.createEntityManagerFactory("pu");
+		instance = EmployeeFacade.getEmployeeFacade(emf);
 	}
 
 	@AfterEach
@@ -47,85 +49,9 @@ public class EmployeeFacadeIT {
 	@Test
 	public void testGetEmployeeFacade() {
 		System.out.println("getEmployeeFacade");
-		EntityManagerFactory _emf = null;
-		EmployeeFacade expResult = null;
-		EmployeeFacade result = EmployeeFacade.getEmployeeFacade(_emf);
-		assertEquals(expResult, result);
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
-	}
-
-	/**
-	 * Test of getEntityManager method, of class EmployeeFacade.
-	 */
-	@Test
-	public void testGetEntityManager() {
-		System.out.println("getEntityManager");
-		EmployeeFacade instance = new EmployeeFacade();
-		EntityManager expResult = null;
-		EntityManager result = instance.getEntityManager();
-		assertEquals(expResult, result);
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
-	}
-
-	/**
-	 * Test of getEmployeeById method, of class EmployeeFacade.
-	 */
-	@Test
-	public void testGetEmployeeById() {
-		System.out.println("getEmployeeById");
-		int id = 0;
-		EmployeeFacade instance = new EmployeeFacade();
-		EmployeeDTO expResult = null;
-		EmployeeDTO result = instance.getEmployeeById(id);
-		assertEquals(expResult, result);
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
-	}
-
-	/**
-	 * Test of getEmployeesByName method, of class EmployeeFacade.
-	 */
-	@Test
-	public void testGetEmployeesByName() {
-		System.out.println("getEmployeesByName");
-		String name = "";
-		EmployeeFacade instance = new EmployeeFacade();
-		List<EmployeeDTO> expResult = null;
-		List<EmployeeDTO> result = instance.getEmployeesByName(name);
-		assertEquals(expResult, result);
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
-	}
-
-	/**
-	 * Test of getAlEmployees method, of class EmployeeFacade.
-	 */
-	@Test
-	public void testGetAlEmployees() {
-		System.out.println("getAlEmployees");
-		EmployeeFacade instance = new EmployeeFacade();
-		List<EmployeeDTO> expResult = null;
-		List<EmployeeDTO> result = instance.getAlEmployees();
-		assertEquals(expResult, result);
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
-	}
-
-	/**
-	 * Test of getEmployeesWithHighestSalary method, of class EmployeeFacade.
-	 */
-	@Test
-	public void testGetEmployeesWithHighestSalary() {
-		System.out.println("getEmployeesWithHighestSalary");
-		int salary = 0;
-		EmployeeFacade instance = new EmployeeFacade();
-		List<EmployeeDTO> expResult = null;
-		List<EmployeeDTO> result = instance.getEmployeesWithHighestSalary(salary);
-		assertEquals(expResult, result);
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
+		EntityManagerFactory _emf = Persistence.createEntityManagerFactory("pu");
+		EmployeeFacade expResult = EmployeeFacade.getEmployeeFacade(_emf);
+		assertEquals(expResult, instance);
 	}
 
 	/**
@@ -134,14 +60,102 @@ public class EmployeeFacadeIT {
 	@Test
 	public void testCreateEmployee() {
 		System.out.println("createEmployee");
-		EmployeeDTO employeeDTO = null;
-		int salary = 0;
-		EmployeeFacade instance = new EmployeeFacade();
-		EmployeeDTO expResult = null;
+		EmployeeDTO employeeDTO = new EmployeeDTO("BobTest", "BobTestVej");
+		int salary = 1000;
 		EmployeeDTO result = instance.createEmployee(employeeDTO, salary);
-		assertEquals(expResult, result);
-		// TODO review the generated test code and remove the default call to fail.
-		fail("The test case is a prototype.");
+		EmployeeDTO expResult = instance.getEmployeeById(result.getId());
+		assertEquals(expResult.getId(), result.getId());
+		assertEquals(expResult.getName(), result.getName());
+		assertEquals(expResult.getAddress(), result.getAddress());
+	}
+
+	/**
+	 * Test of getEmployeeById method, of class EmployeeFacade.
+	 */
+	@Test
+	public void testGetEmployeeById() {
+		System.out.println("getEmployeeById");
+		int salary = 10000;
+		EmployeeDTO edto = new EmployeeDTO("getid", "getidvej");
+		EmployeeDTO expResult = instance.createEmployee(edto, salary);
+		EmployeeDTO result = instance.getEmployeeById(expResult.getId());
+		assertEquals(expResult.getId(), result.getId());
+		assertEquals(expResult.getName(), result.getName());
+		assertEquals(expResult.getAddress(), result.getAddress());
+	}
+
+	/**
+	 * Test of getEmployeesByName method, of class EmployeeFacade.
+	 */
+	@Test
+	public void testGetEmployeesByName() {
+		System.out.println("getEmployeesByName");
+		String name = "getbynametest";
+		EmployeeDTO edto = new EmployeeDTO("getbynametest", "getbynametestvej");
+
+		List<EmployeeDTO> expResult = new ArrayList<>();
+		for (int i = 0; i < 5; i++) {
+			expResult.add(instance.createEmployee(edto, 9000));
+		}
+
+		List<EmployeeDTO> result = instance.getEmployeesByName(name);
+		int resultLength = result.toArray().length;
+		for (int i = 0; i < 5; i++) {
+			assertEquals(expResult.get(i).getId(), result.get(resultLength - 5).getId());
+			assertEquals(expResult.get(i).getName(), result.get(i).getName());
+			assertEquals(expResult.get(i).getAddress(), result.get(i).getAddress());
+			resultLength = resultLength + 1;
+		}
+	}
+
+	/**
+	 * Test of getAlEmployees method, of class EmployeeFacade.
+	 */
+	@Test
+	public void testGetAlEmployees() {
+		System.out.println("getAlEmployees");
+		List<EmployeeDTO> allCurrent = instance.getAlEmployees();
+		EmployeeDTO edto = new EmployeeDTO("getallemployees", "getallemployeesvej");
+		List<EmployeeDTO> expResult = new ArrayList<>();
+
+		allCurrent.forEach(e -> {
+			expResult.add(e);
+		});
+
+		for (int i = 0; i < 5; i++) {
+			expResult.add(instance.createEmployee(edto, 20));
+		}
+		List<EmployeeDTO> result = instance.getAlEmployees();
+
+		for (EmployeeDTO e : result) {
+			assertEquals(expResult.get(e.getId() - 1).getId(), result.get(e.getId() - 1).getId());
+			assertEquals(expResult.get(e.getId() - 1).getName(), result.get(e.getId() - 1).getName());
+			assertEquals(expResult.get(e.getId() - 1).getName(), result.get(e.getId() - 1).getName());
+		}
+	}
+
+	/**
+	 * Test of getEmployeesWithHighestSalary method, of class EmployeeFacade.
+	 */
+	@Test
+	public void testGetEmployeesWithHighestSalary() {
+		System.out.println("getEmployeesWithHighestSalary");
+		int salary = 500000;
+		EmployeeDTO edto = new EmployeeDTO("highsalary", "highsalaryvej");
+		List<EmployeeDTO> expResult = new ArrayList<>();
+		for (int i = 0; i < 5; i++) {
+			expResult.add(instance.createEmployee(edto, salary));
+		}
+
+		List<EmployeeDTO> result = instance.getEmployeesWithHighestSalary(salary);
+
+		int forResult = 5;
+		int forCreated = 0;
+		for (EmployeeDTO e : result) {
+			assertEquals(expResult.get(forCreated).getId(), result.get(e.getId() - forResult));
+			forResult = forResult - 1;
+			forCreated = forCreated + 1;
+		}
 	}
 
 }
